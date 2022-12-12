b42798c591477b811c9fe4ae7af49a029803c287
lukaspetricek <lukas.petricek.cz@gmail.com>
2022-12-12 16:32:17 +0100

    articlerepo i

---
+++ RestAPI-Gardening/src/main/java/API/RestAPIGardening/Repository/ArticleRepository.java
@@ -0,0 +1,16 @@
+package API.RestAPIGardening.Repository;
+
+import API.RestAPIGardening.Model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+//    List<Article> findAll();
+
+}
+
+
