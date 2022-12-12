cdc8b3a7fb090459f0108399032ceb8e183cecef
Radek Cihlář <radekcihlar@radimsu.eu>
2022-12-12 10:09:55 +0100

    Initial commit

---
+++ RestAPI-Gardening/src/main/java/API/RestAPIGardening/RestApiGardeningApplication.java
@@ -0,0 +1,13 @@
+package API.RestAPIGardening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiGardeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiGardeningApplication.class, args);
+	}
+
+}
---
+++ RestAPI-Gardening/src/test/java/API/RestAPIGardening/RestApiGardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package API.RestAPIGardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiGardeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
