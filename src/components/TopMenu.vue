5e30a46bc991664cc5ff85743592bf1fa2f8852f
lukaspetricek <lukas.petricek.cz@gmail.com>
2022-12-15 13:27:25 +0100

    update

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -6,6 +6,7 @@ export default {
 <template>
     <nav class="topMenu">
         <ul>
+            <section id ="container">
             <li><router-link to="/">Home</router-link></li>
             <li><router-link to="/home">Home</router-link></li>
             <!-- <li><router-link :to="{ name: 'Tabs'}">Tabs</router-link></li> -->
@@ -16,21 +17,21 @@ export default {
             <li><router-link to="/home">About us</router-link></li>
             <li><router-link to="/home">Contact</router-link></li>
             <li><a href="https://duckduckgo.com/" target="_blank">DuckDuckGo</a></li>
+            </section>
         </ul>
     </nav>
 </template>
 <style>
 .topMenu {
+    font-family: Arial, Helvetica, sans-serif;
     list-style: none;
-    text-transform: uppercase;
+    /* text-transform: uppercase; */
     font-weight: 900;
-    font-style: italic;
-    font-size: 1rem;
-    line-height:  0.7;
-    margin: 0;
+    line-height:  2.6;
+    margin: -16px;
     padding: 0;
     text-align: center;
-    box-shadow: 0px 1px 10px #000;
+    /* box-shadow: 0px 1px 10px white; */
 }
 .topMenu li {
     display: inline;
@@ -38,11 +39,21 @@ export default {
 .topMenu a {
     text-decoration: none;
     color: black;
-    padding: 30px 30px;
+    padding: 40px 20px;
     display: inline-block;
 }
 .topMenu a:hover {
-    background-color: green;
+    background-color: rgb(123, 166, 67);
     color: white;
+}
+#container {
+    background-image: url('../assets/menu.webp');
+}
+a {
+  transition: color 0.4s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+a:hover {
+  color: #fff;
+  box-shadow: inset 200px 0 0 0 rgb(133, 166, 69);
 }
 </style>
